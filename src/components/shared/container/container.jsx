78a905fb82b20c9607615e04573bb0d2789e5ec3
sartios <sartios@hotmail.com>
2018-02-05 10:01:47 +0200

    fix responsiveness of index page

--- src/components/shared/container/container.jsx
+++ src/components/shared/container/container.jsx
@@ -4,9 +4,14 @@ import styled from 'styled-components';
 const Container = props => {
   const ContainerWrapper = styled.div`
     width: 100%;
+    max-width: 100%;
     height: 900px;
     padding: 0 16px;
     margin-top: 50px;
+
+    @media (max-width: 768px) {
+      padding: 0;
+    }
   `;
 
   return <ContainerWrapper>{props.children}</ContainerWrapper>
